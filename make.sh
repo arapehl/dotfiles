eb07e79dc8f36cc54fc705c248d76b526efe7d1b
Ara Pehlivanian <ara.pehlivanian@gmail.com>
2015-06-26 09:56:47 -0400

    making provision for bash_profile

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="bashrc vimrc custom_git_completion git-completion.bash"
+files="bash_profile bashrc vimrc custom_git_completion git-completion.bash"
 mkdir -p $olddir
 for file in $files; do
   echo $file
