3815e8a1817381de5c6ba356ec6c69ec19ed0ded
Ara Pehlivanian <ara.pehlivanian@gmail.com>
2026-02-27 14:04:50 -0500

    Enhance bootstrap script to manage dotfiles and shell
    Added logic to clone dotfiles repository if not present and set zsh as the default shell.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,22 @@
 # Ensure zsh is installed
 sudo apt-get update -y && sudo apt-get install -y zsh curl
 
+# Clone dotfiles repo permanently if not already present
+if [ ! -d "$HOME/dotfiles" ]; then
+  git clone https://github.com/arapehl/dotfiles.git "$HOME/dotfiles"
+fi
+
+cd "$HOME/dotfiles"
+
 # Install software
 source ./install.zsh
 
 # ZSH
-ln -sfn ~/dotfiles/zsh ~/.zsh
-ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sfn "$HOME/dotfiles/zsh" "$HOME/.zsh"
+ln -sf "$HOME/dotfiles/zsh/zshrc" "$HOME/.zshrc"
 
 # Git
-ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf "$HOME/dotfiles/gitconfig" "$HOME/.gitconfig"
+
+# Set zsh as default shell
+sudo chsh -s $(which zsh) $(whoami)
